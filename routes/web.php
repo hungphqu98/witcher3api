54dbc3a8270cbe259311007985cd81fe755354bd
hungphqu98 <pqh2405@gmail.com>
2021-03-12 18:27:56 +0700

    added react

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/playground', function () {
     return view('playground');
 });
+Route::view('/{path?}', 'app');
